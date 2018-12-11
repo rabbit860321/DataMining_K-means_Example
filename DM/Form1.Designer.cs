23e3f05e5327212c04405899dd7f6643e0d6f00d
rabbit860321 <rabbit860321@gmail.com>
2018-12-11 22:34:35 +0800

    20181211

---
+++ DM/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DataMining
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(23, 21);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(664, 368);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "找中心點";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(188, 409);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "分群";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(353, 409);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 48);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "重新";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 540);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ DM/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace DataMining
+{
+    public partial class Form1 : Form
+    {
+        Series ser = new Series();
+        Series ACenter = new Series("A center");
+        Series BCenter = new Series("B center");
+        Series CCenter = new Series("C center");
+
+        List<double> dislist = new List<double>();  //距離陣列
+
+
+
+        public Form1() 
+        {
+            InitializeComponent();
+            setchart();
+
+            ser.ChartType = SeriesChartType.Point; //設定Point種類
+            ser.MarkerStyle = MarkerStyle.Circle;  //Point圓圈表示
+            ser.MarkerSize = 10;  //圓圈大小  
+
+
+            setAcenterpoint(ACenter);
+            setBcenterpoint(BCenter);
+            setCcenterpoint(CCenter);
+            
+            ser.Points.AddXY(2, 10);
+            ser.Points.AddXY(2, 5);
+            ser.Points.AddXY(8, 4);
+
+            ser.Points.AddXY(5, 8);
+            ser.Points.AddXY(7, 5);
+            ser.Points.AddXY(6, 4);
+
+            ser.Points.AddXY(1, 2);
+            ser.Points.AddXY(4, 9);
+             
+
+            this.chart1.Series.Add(ser);//將線畫在圖上
+            this.chart1.Series.Add(ACenter);
+            this.chart1.Series.Add(BCenter);
+            this.chart1.Series.Add(CCenter);
+
+            ser.Points[0].Color = Color.Blue;
+            ser.Points[1].Color = Color.Blue;
+            ser.Points[2].Color = Color.Blue;
+            ser.Points[3].Color = Color.Red;
+            ser.Points[4].Color = Color.Red;
+            ser.Points[5].Color = Color.Red;
+            ser.Points[6].Color = Color.Green;
+            ser.Points[7].Color = Color.Green;        
+        }
+
+
+        public void setchart() //繪圖
+        {
+            //---------------------
+            chart1.Series.Clear();  //每次使用此function前先清除圖表
+            chart1.ChartAreas[0].AxisX.Minimum = 0;  //設定X軸最小值
+            chart1.ChartAreas[0].AxisX.Maximum = 10; //設定X軸最大值
+            chart1.ChartAreas[0].AxisY.Minimum = 0;  //設定Y軸最小值
+            chart1.ChartAreas[0].AxisY.Maximum = 10; //設定Y軸最大值
+            chart1.ChartAreas[0].AxisX.MajorTickMark.Interval = 1;  //X軸刻度線的間隔
+            chart1.ChartAreas[0].AxisY.MajorTickMark.Interval = 1;  //Y軸刻度線的間隔
+            
+        }
+
+        public void setAcenterpoint(Series point)
+        {
+            point.Color = Color.Blue; //設定Point顏色           
+            point.ChartType = SeriesChartType.Point; //設定Point種類
+            point.MarkerStyle = MarkerStyle.Star5;  //Point圓圈表示
+            point.MarkerSize = 20;  //圓圈大小  
+        }
+        public void setBcenterpoint(Series point)
+        {
+            point.Color = Color.Red; //設定Point顏色           
+            point.ChartType = SeriesChartType.Point; //設定Point種類
+            point.MarkerStyle = MarkerStyle.Star5;  //Point圓圈表示
+            point.MarkerSize = 20;  //圓圈大小  
+        }
+        public void setCcenterpoint(Series point)
+        {
+            point.Color = Color.Green; //設定Point顏色           
+            point.ChartType = SeriesChartType.Point; //設定Point種類
+            point.MarkerStyle = MarkerStyle.Star5;  //Point圓圈表示
+            point.MarkerSize = 20;  //圓圈大小  
+        }
+
+        public void button1_Click(object sender, EventArgs e) 
+        {
+            ACenter.Points.Clear();
+            BCenter.Points.Clear();
+            CCenter.Points.Clear();
+            double Ax = 0;               //Ax A中心 x座標
+            double Ay = 0;               //Ay A中心 y座標
+            double Bx = 0;
+            double By = 0;
+            double Cx = 0;
+            double Cy = 0;
+
+            for (int i = 0; i < ser.Points.Count; i++)
+            {
+                if(ser.Points[i].Color == Color.Blue)
+                {
+                    Ax += ser.Points[i].XValue;
+                    Ay += ser.Points[i].YValues[0];
+                }
+            }
+            Ax /= 3;                                //中心座標 = (x1+x2+x3)/3
+            Ay /= 3;
+            ACenter.Points.AddXY(Ax, Ay);
+            
+
+            for (int i = 0; i < ser.Points.Count; i++)
+            {
+                if (ser.Points[i].Color == Color.Red)
+                {
+                    Bx += ser.Points[i].XValue;
+                    By += ser.Points[i].YValues[0];
+                }
+            }
+            Bx /= 3;
+            By /= 3;
+            BCenter.Points.AddXY(Bx, By);
+            
+
+            
+            for (int i = 0; i < ser.Points.Count; i++)
+            {
+                if (ser.Points[i].Color == Color.Green)
+                {
+                    Cx += ser.Points[i].XValue;
+                    Cy += ser.Points[i].YValues[0];
+                }
+            }
+            Cx /= 2;
+            Cy /= 2;
+            CCenter.Points.AddXY(Cx, Cy);
+            
+        }
+
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ser.Points.Count; i++)              //讓所有點設為黑色
+            {
+                ser.Points[i].Color = Color.Black;
+            }
+
+            for (int i = 0; i < ser.Points.Count; i++)                                                            //存放每一點到A中心的距離
+            {
+                dislist.Add(Math.Sqrt(Math.Pow(ser.Points[i].XValue - ACenter.Points[0].XValue, 2)
+                    + Math.Pow(ser.Points[i].YValues[0] - ACenter.Points[0].YValues[0], 2)));
+            }
+            MessageBox.Show("" + dislist[0] + "\n" + dislist[1] + "\n" + dislist[2] + "\n" + dislist[3] + "\n" + dislist[4] + "\n" + dislist[5] + "\n" + dislist[6] + "\n" + dislist[7] + "\n");
+
+            for (int i = 0; i < dislist.Count; i++)                        
+            {
+                if(dislist.Min() == dislist[i])             //找出距離最短的第一點
+                {
+                    ser.Points[i].Color = Color.Blue;
+                    dislist[i] = dislist.Max();
+                    break;
+                }
+            }
+            for (int i = 0; i < dislist.Count; i++)
+            {
+                if (dislist.Min() == dislist[i])             //第二點
+                {
+                    ser.Points[i].Color = Color.Blue;
+                    dislist[i] = dislist.Max();
+                    break;
+                }
+            }
+            for (int i = 0; i < dislist.Count; i++)
+            {
+                if (dislist.Min() == dislist[i])                //第三點
+                {
+                    ser.Points[i].Color = Color.Blue;
+                    dislist[i] = dislist.Max();
+                    break;
+                }
+            }
+            dislist.Clear();               
+
+            for (int i = 0; i < ser.Points.Count; i++)                                                            
+            {
+                if(ser.Points[i].Color == Color.Black)                       //只計算尚未分群的點與B中心距離
+                {
+                    dislist.Add(Math.Sqrt(Math.Pow(ser.Points[i].XValue - BCenter.Points[0].XValue, 2)
+                    + Math.Pow(ser.Points[i].YValues[0] - BCenter.Points[0].YValues[0], 2)));
+                }
+                else
+                {
+                    dislist.Add(100000);                                     //若以分群 存放1000000
+                }
+                
+            }
+            //MessageBox.Show("" + dislist[0] + "\n" + dislist[1] + "\n" + dislist[2] + "\n" + dislist[3] + "\n" + dislist[4] + "\n" + dislist[5] + "\n" + dislist[6] + "\n" + dislist[7] + "\n");
+
+            for (int i = 0; i < dislist.Count; i++)                   //第一點
+            {
+                if (dislist.Min() == dislist[i])
+                {
+                    ser.Points[i].Color = Color.Red;
+                    dislist[i] = dislist.Max();
+                    break;
+                }
+            } 
+            for (int i = 0; i < dislist.Count; i++)                  //第二點
+            {
+                if (dislist.Min() == dislist[i])
+                {
+                    ser.Points[i].Color = Color.Red;
+                    dislist[i] = dislist.Max();
+                    break;
+                }
+            }
+            for (int i = 0; i < dislist.Count; i++)                //第三點
+            {
+                if (dislist.Min() == dislist[i])
+                {
+                    ser.Points[i].Color = Color.Red;
+                    dislist[i] = dislist.Max();
+                    break;
+                }
+            }
+            dislist.Clear();
+
+            for (int i = 0; i < ser.Points.Count; i++)                           //其餘就是C群
+            {
+                if(ser.Points[i].Color == Color.Black)
+                {
+                    ser.Points[i].Color = Color.Green;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ser.Points[0].Color = Color.Blue;
+            ser.Points[1].Color = Color.Blue;
+            ser.Points[2].Color = Color.Blue;
+            ser.Points[3].Color = Color.Red;
+            ser.Points[4].Color = Color.Red;
+            ser.Points[5].Color = Color.Red;
+            ser.Points[6].Color = Color.Green;
+            ser.Points[7].Color = Color.Green;
+
+            ACenter.Points.Clear();
+            BCenter.Points.Clear();
+            CCenter.Points.Clear();
+        }
+    }
+}
